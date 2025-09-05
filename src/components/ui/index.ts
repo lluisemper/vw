6839be1c714786533f5980c442d2a8c1ef384809
lluisemper <58423269+lluisemper@users.noreply.github.com>
2025-09-05 18:26:26 +0200

    #14 module not loaded in ci

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,4 +1,4 @@
 export { LoadingSpinner } from "./LoadingSpinner";
 export { LoadingSkeleton } from "./LoadingSkeleton";
 export { ErrorMessage } from "./ErrorMessage";
-export * from "./modal";
+export * from "./modal/index";
